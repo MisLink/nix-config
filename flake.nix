058129a99a0d78c5b99f085eb8819896eb85caf5
MisLink <gjq.uoiai@outlook.com>
2025-04-28 15:22:57 +0800

    Add new key
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- flake.nix
+++ flake.nix
@@ -7,15 +7,6 @@
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
