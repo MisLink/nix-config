4a345ccdc897f6c12a9568a20217ee1ac3bca3cd
MisLink <gjq.uoiai@outlook.com>
2026-03-21 02:04:33 +0800

    Update

--- home/jujtsu.nix
+++ home/jujtsu.nix
@@ -20,7 +20,7 @@
     }
     // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
       signing = {
-        behavior = "own";
+        behavior = "drop";
         backend = "ssh";
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnH3JJcsZnksInIdffC18IkcI2IGxnvyQBv3j+/MHsm";
         backends.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
