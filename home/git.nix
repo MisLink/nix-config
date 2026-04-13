aa05eb773cc4490f1fbba5e96920da33be93d128
MisLink <gjq.uoiai@outlook.com>
2026-04-13 16:14:41 +0800

    chore: update signer

--- home/git.nix
+++ home/git.nix
@@ -95,6 +95,8 @@
       format = "ssh";
       signByDefault = true;
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnH3JJcsZnksInIdffC18IkcI2IGxnvyQBv3j+/MHsm";
+    }
+    // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
       signer = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
     };
   };
--- home/jujtsu.nix
+++ home/jujtsu.nix
@@ -28,6 +28,8 @@
         behavior = "drop";
         backend = "ssh";
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnH3JJcsZnksInIdffC18IkcI2IGxnvyQBv3j+/MHsm";
+      }
+      // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
         backends.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
       };
     };
