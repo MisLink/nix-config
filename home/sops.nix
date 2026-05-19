d220e1ae14adee0b653726f7d5a0a67cd13aac3a
MisLink <gjq.uoiai@outlook.com>
2026-05-19 11:55:59 +0800

    Update

--- home/sops.nix
+++ home/sops.nix
@@ -1,15 +1,25 @@
 { pkgs, homedir, ... }:
+let
+  op =
+    if pkgs.stdenv.hostPlatform.system == "aarch64-darwin" then
+      "/opt/homebrew/bin/op"
+    else
+      "/usr/local/bin/op";
+  opTokenFile = "${homedir}/.config/op/sops-service-account-token";
+  sopsAgeKeyCmd = pkgs.writeShellScript "sops-age-key-cmd" ''
+    set -eu
+    if [ -z "''${OP_SERVICE_ACCOUNT_TOKEN:-}" ]; then
+      export OP_SERVICE_ACCOUNT_TOKEN="$(cat "${opTokenFile}")"
+    fi
+    exec ${op} read "op://SOPS/primary/private_key?ssh-format=openssh"
+  '';
+in
 {
   sops = {
     # age.keyFile = "${homedir}/.config/sops/age/keys.txt";
     age.keyFile = "";
     environment = {
-      SOPS_AGE_SSH_PRIVATE_KEY_CMD = ''${
-        if pkgs.stdenv.hostPlatform.system == "aarch64-darwin" then
-          "/opt/homebrew/bin/op"
-        else
-          "/usr/local/bin/op"
-      } read "op://Personal/primary/private_key?ssh-format=openssh"'';
+      SOPS_AGE_SSH_PRIVATE_KEY_CMD = "${sopsAgeKeyCmd}";
     };
     defaultSopsFile = ../secrets/secrets.yaml;
     secrets.git = { };
