795903c25579a342b286085744c507eb6a11002e
MisLink <gjq.uoiai@outlook.com>
2026-06-20 02:53:53 +0800

    Update
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- home/sops.nix
+++ home/sops.nix
@@ -1,29 +1,7 @@
 { pkgs, homedir, ... }:
-let
-  op =
-    if pkgs.stdenv.hostPlatform.system == "aarch64-darwin" then
-      "/opt/homebrew/bin/op"
-    else
-      "/usr/local/bin/op";
-  opTokenFile = "${homedir}/.config/op/sops-service-account-token";
-  sopsAgeKeyCmd = pkgs.writeShellScript "sops-age-key-cmd" ''
-    set -eu
-
-    if [ -z "''${OP_SERVICE_ACCOUNT_TOKEN:-}" ]; then
-      IFS= read -r OP_SERVICE_ACCOUNT_TOKEN < "${opTokenFile}"
-      export OP_SERVICE_ACCOUNT_TOKEN
-    fi
-
-    exec ${op} read "op://key/sops/private_key?ssh-format=openssh"
-  '';
-in
 {
   sops = {
-    # age.keyFile = "${homedir}/.config/sops/age/keys.txt";
-    age.keyFile = "";
-    environment = {
-      SOPS_AGE_SSH_PRIVATE_KEY_CMD = "${sopsAgeKeyCmd}";
-    };
+    age.keyFile = "${homedir}/.config/sops/age/keys.txt";
     defaultSopsFile = ../secrets/secrets.yaml;
     secrets.git = { };
     secrets.ssh_hosts = { };
